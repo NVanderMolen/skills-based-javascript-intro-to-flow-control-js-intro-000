c02a43997805829b0cfeeba255f3b13f23fd2351
NICHOLAS R VANDERMOLEN <tree.vandermolen@gmail.com>
2020-03-28 19:33:04 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -11,12 +11,7 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
- if (age <= 12)
-  return ("You are a kid");
- else if (age >= 13 && <= 19)
-  return ("You are a teenager!");
- else
-  return ("You are a grownup")
+
 }
 
 function ternaryTeenager(age) {
